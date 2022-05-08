e7b1a2b74c7aa46abf0d4dec1cab85276f59bdac
Sean <seanbopo@gmail.com>
2022-05-08 10:29:50 +0800

    Initial Commit

---
+++ KaohsiungNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaohsiungNews
+//
+//  Created by ChaoBo-Shang on 2022/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
